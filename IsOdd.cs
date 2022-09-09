adb4ba3e04d7fb8f3d71a7a998d76818349ea6c5
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-09 10:04:11 +0200

    is odd

---
+++ IsOdd.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class IsOdd_Exercice
+    {
+        public static bool IsOdd(int a)
+        {
+            {
+                if (a / 2 == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
