8fc0f853244f0af9b2a150c62ca76ca9afd06b93
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-09 13:39:56 +0200

    capitalize

---
+++ Capitalize.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+    public class Capitalize_Exercice
+    {
+        public static string Capitalize(string str)
+        {
+            return str.ToUpper();
+        }
+    }
+}
