92c60956d9ed6c264c82fec932dba63f8364d01c
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-09 14:07:16 +0200

    Traffic light

---
+++ NextTrafficLightColor.cs
@@ -0,0 +1,14 @@
+﻿using CSharpDiscovery.Models;
+
+namespace CSharpDiscovery.Quest01
+{
+    public class NextTrafficLightColor_Exercice
+    {
+        public static TrafficLightColor GetNextTrafficLightColor(TrafficLightColor currentColor)
+        {
+            return currentColor == TrafficLightColor.None ? TrafficLightColor.None :
+                currentColor == TrafficLightColor.Green ? TrafficLightColor.Orange :
+                currentColor == TrafficLightColor.Orange ? TrafficLightColor.Red : TrafficLightColor.Green;
+        }
+    }
+}
