67273c5cf8c2804fd685d8d08e99b64de114ee9c
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-12 08:54:02 +0200

    find last index

--- FindLastIndex.cs
+++ FindLastIndex.cs
@@ -7,6 +7,8 @@ namespace CSharpDiscovery.Quest02
     {
         public static int? FindLastIndex(int[] tab, int a)
         {
+            if (tab == null || a == null) return null;
+            
             return Array.LastIndexOf(tab, a) == -1 ? null : Array.LastIndexOf(tab, a);
         }
     }
