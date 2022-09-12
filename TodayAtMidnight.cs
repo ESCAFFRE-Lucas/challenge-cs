37f34d708f0009254cfcb1ba8fb45e25d4489445
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-12 08:22:52 +0200

    today at midnight

---
+++ TodayAtMidnight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class TodayAtMidnight_Exercice
+    {
+        public static DateTime TodayAtMidnight()
+        {
+            var dateAndTime = DateTime.Now;
+            return dateAndTime.Date;
+        }
+    }
+}
