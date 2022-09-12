3df06b9c6483ec1e09de485efa6090adc38916a9
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-12 11:22:01 +0200

    last chance

--- SecretSanta.cs
+++ SecretSanta.cs
@@ -1,45 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CSharpDiscovery.Quest02
 {
-    public class SecretSanta_Exercice
+  public class SecretSanta_Exercice
+  {
+    public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
     {
-        public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
+      Dictionary<string, string> dic = new Dictionary<String, String>();
+
+      if (people.Count == 2)
+      {
+        dic.Add(people.ElementAt(0), people.ElementAt(1));
+        dic.Add(people.ElementAt(1), people.ElementAt(0));
+        return dic;
+      }
+      
+      Random rnd = new Random();
+      
+      for (int i = 0; i < people.Count; i++)
+      {
+        string person = people.ElementAt(i);
+        int indexOfPerson = rnd.Next(0, people.Count);
+
+        if (dic.ContainsKey(person) == false)
         {
-            Dictionary<string, string> dic = new Dictionary<string, string>();
-            var rand = new Random();
-
-            for (int i = 0; i <= people.Count + 1; i++)
-            {
-                int randomNumber = rand.Next(0, people.Count);
-                int randomNumber2 = rand.Next(0, people.Count);
-
-                if (randomNumber == randomNumber2 && people.Count != 2)
-                {
-                    randomNumber2 = rand.Next(0, people.Count);
-                }
-				
-				if (randomNumber == randomNumber2 && people.Count == 2)
-				{
-					if (randomNumber == 1) 
-					{
-						randomNumber2 = 0;
-					} 
-					if (randomNumber == 0) 
-					{
-						randomNumber2 = 1;
-					}
- 				}
-                var person1 = people.ElementAt(randomNumber);
-                var person2 = people.ElementAt(randomNumber2);
-                people.Remove(person1);
-                people.Remove(person2);
-                dic.Add(person1, person2);
-            }
-            return dic;
+          dic.Add(person, people.ElementAt(indexOfPerson));
         }
+      }
+      return dic;
     }
+  }
 }
