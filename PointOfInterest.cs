e2e87f479f139fae85d9cb77f7cd196683bf8cb0
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-14 09:11:06 +0200

    Point of Interest part 3

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -8,6 +8,8 @@ namespace CSharpDiscovery.Quest03
         public double Longitude { get; set; } = 0;
         public string Name { get; set; } = "";
 
+        public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+
         public PointOfInterest()
         {
             Latitude = 44.854186;
@@ -20,6 +22,11 @@ namespace CSharpDiscovery.Quest03
             Name = name;
             Latitude = latitude;
             Longitude = longitude;
+        }
+
+        public string GetGoogleMapsUrl()
+        {
+            return ($"https://www.google.com/maps/place/{Name.Replace(" ","+")}/@{Latitude},{Longitude},15z/");
         }
     }
 }
