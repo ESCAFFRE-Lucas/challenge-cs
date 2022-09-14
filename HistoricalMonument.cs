6388dbf8b0685628553f52b94ad1293958c654dc
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-14 10:34:19 +0200

    Historical monument is not good

--- HistoricalMonument.cs
+++ HistoricalMonument.cs
@@ -5,6 +5,8 @@ namespace CSharpDiscovery.Quest03
     public class HistoricalMonument : PointOfInterest
     {
         public int BuildYear { get; set; } = 0;
+        
+        public HistoricalMonument(){}
 
         public HistoricalMonument(string name, double latitude, double longitude, int buildYear)
         {
