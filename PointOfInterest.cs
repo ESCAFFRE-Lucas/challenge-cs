96239b98091da36f8a5b86c8b0969d860b39e7a1
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-14 09:03:12 +0200

    point of interest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,10 +4,23 @@ namespace CSharpDiscovery.Quest03
 {
     public class PointOfInterest
     {
-        public double Latitude { get; } = 0;
-        public double Longitude { get; } = 0;
-        public string Name { get; } = "";
-        public static string GoogleMapsUrlTemplate { get; }= "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+        public double Latitude { get; set; } = 0;
+        public double Longitude { get; set; } = 0;
+        public string Name { get; set; } = "";
+
+        public PointOfInterest()
+        {
+            Latitude = 44.854186;
+            Longitude = -0.5663056;
+            Name = "Bordeaux Ynov Campus";
+        }
+
+        public PointOfInterest(string name, double latitude, double longitude)
+        {
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
     }
 }
 
