ea6fc5d9fccc02547b2c62d85c2dddb29dc148ab
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-14 08:32:35 +0200

    work ?

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,9 +4,9 @@ namespace CSharpDiscovery.Quest03
 {
     public class PointOfInterest
     {
-        public double Latitude;
-        public double Longitude;
-        public string Name;
+        public double Latitude = 0;
+        public double Longitude = 0 ;
+        public string Name = "";
         public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
     }
 }
