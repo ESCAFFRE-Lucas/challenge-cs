000f90e4b0bcf0adcb4e5c7d7dea999e1a118e5e
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-16 09:17:49 +0200

    carInterface ?

---
+++ CarInterfaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest04
+{
+    interface IThermalCar
+    {
+        public int FuelLevel { get; set; } = 0;
+        void FillUp();
+        int GetFuelLevel();
+    }
+
+    interface IElectricCar
+    {
+        public int BatteryLevel { get; set; } = 0;
+        void Recharge();
+        int GetBatteryLevel();
+    }
+}
