560b8b57b6fe1244375edf4a3b0124473ef22ada
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-16 08:34:56 +0200

    Car ?

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest04
+{
+    public class Car : Vehicule
+    {
+        public string Model { get; set; } = "";
+
+        public Car()
+        {
+            string Brand = "Unknown";
+        }
+
+        public Car(string model, string brand, string color, int currentSpeed = 0)
+        {
+            Model = model;
+            Brand = brand;
+            Color = color;
+            CurrentSpeed = currentSpeed;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {Brand} {Model}";
+        }
+    }
+}
