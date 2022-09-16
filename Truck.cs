69ca490c5accb1828b855801af59b1456aa5742a
LESCAFFRE <lucas.escaffre@ynov.com>
2022-09-16 09:12:59 +0200

    Truck ?

---
+++ Truck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest04
+{
+    public class Truck : Vehicule
+    {
+        public int Tonnage { get; set; } = 0;
+
+        public Truck()
+        {
+            Tonnage = 0;
+        }
+
+        public Truck(int tonnage, string brand, string color, int currentSpeed = 0)
+        {
+            Tonnage = tonnage;
+            Brand = brand;
+            Color = color;
+            CurrentSpeed = currentSpeed;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {Brand} {Tonnage}T Truck";
+        }
+        
+        public override void Accelerate(int Speed)
+        {
+            if (CurrentSpeed + Speed > 100)
+            {
+                CurrentSpeed = 100;
+            }
+            else
+            {
+                CurrentSpeed += Speed;
+            }
+        }
+
+        public override void Brake(int BrakePower)
+        {
+            if (CurrentSpeed - BrakePower < 0)
+            {
+                CurrentSpeed = 0;
+            }
+            else
+            {
+                CurrentSpeed -= BrakePower;
+            }
+        }
+    }
+}
